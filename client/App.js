5393de0aa1962e34bec6b4e1786d9758f9cfbb4e
crisvalhas <cristianrubiles@gmail.com>
2022-07-05 23:56:10 -0300

    Redux

--- client/App.js
+++ client/App.js
@@ -1,20 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { Provider } from 'react-redux';
+import store from './Redux/Store/index.js'
+import Main from './Src/Screens/Main.jsx';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider store={store}>
+      <Main/>
+    </Provider>  
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
