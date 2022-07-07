085f6417198a09b1e65e1d793868ee3e131fd6d0
vlmnst <vale-mansueto@hotmail.com>
2022-07-07 11:15:32 -0300

    ruta y funcion getproductbyname

--- api/src/routes/products.js
+++ api/src/routes/products.js
@@ -6,6 +6,7 @@ const { getAllProducts, createProduct, totalProducts, deleteProduct, buyProduct,
 productsRouter.get('/getAll', getAllProducts);
 productsRouter.get('/count', totalProducts);
 productsRouter.get('/category/:category', getProductsByCategory);
+productsRouter.get('/search/:name', getProductsByName);
 productsRouter.post('/create', createProduct);
 productsRouter.post('/buy', buyProduct);
 productsRouter.delete('/delete', deleteProduct);
