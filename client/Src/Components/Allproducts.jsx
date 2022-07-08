da36e24831981435867a05caffa88db961bdb3da
crisvalhas <cristianrubiles@gmail.com>
2022-07-08 15:21:24 -0300

    Fix-Pickers

--- client/Src/Components/Allproducts.jsx
+++ client/Src/Components/Allproducts.jsx
@@ -1,77 +1,71 @@
-import React, { useState, useEffect,useRef } from "react";
+import React, { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { View, Text, FlatList, StyleSheet, useWindowDimensions } from "react-native";
+import { View, Text, FlatList, StyleSheet } from "react-native";
 import { getByPrice, getProductsByCategory, clearCache } from '../../Redux/Slice';
 import ProductCard from './ProductCard.jsx';
-import { Picker } from '@react-native-picker/picker';
+import DropDownPicker from 'react-native-dropdown-picker';
+
 
 const Allproducts = ({ route }) => {
     //----------dispatch-------------
     let item = route.params
     let Products = useSelector((state) => state.ALL_PRODUCTS.allProductsFiltered);
-    let [categories,setCategories] = useState(useSelector((state) => state.ALL_PRODUCTS.categories));
-    const pickerRef = useRef();
+    let [categories, setCategories] = useState(useSelector((state) => state.ALL_PRODUCTS.categories));
     const dispatch = useDispatch();
     const [state, setState] = useState({
         title: "AllProducts",
     });
     useEffect(() => { dispatch(getProductsByCategory(item)); }, [dispatch]);
-    useEffect(() => { return dispatch(clearCache()); }, [dispatch]);
+    useEffect(() => { return () => { dispatch(clearCache()) } }, [dispatch]);
+    //----------prueba-------------
+    const [openitems, setOpenitems] = useState(false);
+    const [openprice, setOpenprice] = useState(false);
+    const [valueitems, setValueitems] = useState(null);
+    const [valueprice, setValueprice] = useState(null);
+    let pickerSort= [{label: "higher", value: "higher"},{label: "lower", value:"lower"}]
+    let pickerItems = [];
+    categories.length ? (
+        categories.map((c, index) => (
+            pickerItems.push({ label: c, value: c })
+        ))) : null
     //----------actions-------------
     function handleCategory(e) {
         setState({
             ...state,
-            title: e
+            title: e.value
         })
-        dispatch(getProductsByCategory(e));
+        dispatch(getProductsByCategory(e.value));
     };
     function handlePrice(e) {
-        dispatch(getByPrice(e));
+        dispatch(getByPrice(e.value));
     };
-    function open() {
-        pickerRef.current.focus();
-    };
-    function close() {
-        pickerRef.current.blur();
-    };
-
     return (
         <View style={styles.container}>
             <Text style={styles.title}>{state.title}</Text>
             <View style={styles.selects}>
                 <View style={styles.selects}>
                     {/* ------------Select category------------- */}
-                    <Text >Filter by:  </Text>
-                    <Picker
-                        defaultValue ={null} 
-                        ref={pickerRef}
-                        multiple={true}
-                        selectedValue={categories}
-                        onValueChange={(itemValue, itemIndex) =>
-                        handleCategory(itemValue)
-                        }>
-                            <Picker.Item label={null}  value={null} />
-                            {categories.length ? (
-                            categories.map((c, index) => (
-                                <Picker.Item label={c} value={c} key={index}/>
-                            )
-                            )
-                        ) : (null)}
-                    </Picker>
+                    <View><Text >Filter by:  </Text></View>
+                    <DropDownPicker
+                        open={openitems}
+                        value={valueitems}
+                        items={pickerItems}
+                        setOpen={setOpenitems}
+                        setValue={setValueitems}
+                        onSelectItem={(value) => handleCategory(value)}
+                    />
                 </View>
                 <View style={styles.selects}>
                     {/* ------------order By Price------------- */}
-                    <Text >Order by:  </Text>
-                    <Picker
-                        defaultValue ={null} 
-                        ref={pickerRef}
-                        multiple={true}
-                        onValueChange={(itemValue, itemIndex) =>
-                            handlePrice(itemValue)
-                        }>  <Picker.Item label={null}  value={null} />
-                            <Picker.Item label="higher" value="higher" />
-                            <Picker.Item label="lower" value="lower" />
-                    </Picker>
+                    <View><Text >Order by:  </Text></View>
+                    <DropDownPicker
+                        open={openprice}
+                        value={valueprice}
+                        items={pickerSort}
+                        setOpen={setOpenprice}
+                        setValue={setValueprice}
+                        onSelectItem={(value) => handlePrice(value)}
+                    />
                     {/* ------------Products List------------- */}
                 </View>
             </View>
@@ -92,8 +86,8 @@ const Allproducts = ({ route }) => {
 
 const styles = StyleSheet.create({
     container: { flex: 1 },
-    selects: { flexDirection: "row", padding: 5, justifyContent: "space-evenly" },
-    flatList: { marginTop: 10, padding: 5, width: useWindowDimensions, },
+    selects: { flexDirection: "row", padding: 20, justifyContent: "space-evenly", },
+    flatList: { marginTop: 10, padding: 5 },
     title: { fontSize: 20, padding: 5, marginLeft: 10, }
 })
 
