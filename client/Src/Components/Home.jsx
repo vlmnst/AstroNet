05254086940114198aa343bc3492700289cfe533
vlmnst <vale-mansueto@hotmail.com>
2022-07-08 23:34:14 -0300

    componente navbar

--- client/Src/Components/Home.jsx
+++ client/Src/Components/Home.jsx
@@ -1,11 +1,11 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { View,Text } from "react-native";
+import { View,Text, SafeAreaView, StyleSheet, StatusBar } from "react-native";
 import { getAllProducts} from '../../Redux/Slice';
 import Allproducts from "./Allproducts";
 import Categories from "./Categories";
 import Banner from "./Banner";
-
+import NavBar from "./NavBar";
 
 const Home = ({ navigation }) => {
     //----------ejemplo dispatch-------------
@@ -17,11 +17,19 @@ const Home = ({ navigation }) => {
 
     return (
         <View>
+            <SafeAreaView style={styles.AndroidSafeArea} >
+            <NavBar/>         
             <Banner/>
             <Categories {...navigation}/>
+           </SafeAreaView> 
         </View>
     );
 };
 
+const styles = StyleSheet.create({
+    AndroidSafeArea: {
+      paddingTop: StatusBar.currentHeight + 10,
+    },
+  });
 
 export default Home;
